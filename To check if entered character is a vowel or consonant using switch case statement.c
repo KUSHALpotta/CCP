ae9011f7097e946c7ed08f08551565e540c05129
KUSHALpotta <83854417+KUSHALpotta@users.noreply.github.com>
2021-06-14 09:35:49 +0530

    Create To check if entered character is a vowel or consonant using switch case statement.c
    In this user defined program we check if the given character is a vowel or consonant using switch case statement.

---
+++ To check if entered character is a vowel or consonant using switch case statement.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+
+    printf("enter the character : ");
+    scanf("%c",&ch);
+
+    switch(ch)
+    {
+        case 'a' :
+            printf("The character is a vovel");
+            break;
+        case 'e' :
+            printf("The character is a vovel");
+            break;
+        case 'i' :
+            printf("The character is a vovel");
+            break;
+        case 'o' :
+            printf("The character is a vovel");
+            break;
+        case 'u' :
+            printf("The character is a vovel");
+            break;
+        case 'A' :
+            printf("The character is a vovel");
+            break;
+        case 'E' :
+            printf("The character is a vovel");
+            break;
+        case 'I' :
+            printf("The character is a vovel");
+            break;
+        case 'O' :
+            printf("The character is a vovel");
+            break;
+        case 'U' :
+            printf("The character is a vovel");
+            break;
+        default:
+            printf("The given character is not a vovel");
+    }
+    return 0;
+
+}
